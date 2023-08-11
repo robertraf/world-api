44cdb0240176d974a5bb300417b39750264e92d7
Roberto Ramírez <robertrafyt2007@gmail.com>
2023-08-11 10:48:31 -0600

    fix: country code format

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
 import { Country } from "./types"
 const data = require("./data.json") as { [code: string]: Country }
 
-export const findCountryByCode = (code: string): Country | undefined => data[code];
+export const findCountryByCode = (code: string): Country | undefined => data[code.toUpperCase()];
